b6dca188eda7e6a70cd39ca41c9fbbaa7ab26237
Alex <anfauglit@mail.ru>
2021-01-12 04:08:10 +0300

    domain, range search using hash table

--- main.c
+++ main.c
@@ -3,11 +3,54 @@
 #include <string.h>
 #include <errno.h>
 
+#define SIZE 20
+
 /* TODO 
+ * refactor the code: put structure definitions and function declaration into a separate header file, we are working on a library after all!
+ * add memory deallocation for hash Table
+ * look the Internet for a boilder plate solution (function) for handling errors assosiated with failed memory allocation
  * read data from a file or generated randomly
  * partition cartesian product into a set of equivalence classes. Equivalence
  * relation: have the same first coordinate
 */
+
+/* By using hashTable added implementation of finding domain and range of a relation
+ * right now I see this solution as extremely elegant
+*/
+
+typedef struct node {
+	int data;
+	int key;
+} Node;
+
+//hash function
+int getHash(int key) {
+	return key % SIZE;
+}
+
+void insert(Node **hashArray, int key, int data) {
+	Node *newNode = malloc(sizeof(Node));
+	newNode->key = key;
+	newNode->data = data;
+
+	int hashValue = getHash(key);
+
+	while (hashArray[hashValue] != NULL && hashArray[hashValue]->key != key) {
+		hashValue++;
+		hashValue %= SIZE;
+	}
+
+	hashArray[hashValue] = newNode;
+}
+
+void printHashTable(Node **hashArray) {
+	for (int i = 0; i < SIZE; ++i) {
+		if (hashArray[i] == NULL)
+			printf("null ");
+		else
+			printf("%4i ", hashArray[i]->data);
+	}
+}
 
 typedef struct pair {
 	int x;
@@ -74,12 +117,17 @@ void printSet(const Set *set) {
 }
 
 int main (int argc, char **argv) {
+	Node **hashTable = calloc(SIZE, sizeof(Node *));
+	printHashTable(hashTable);
+	printf("\n");
+
 	int setA[] = {1, 2, 3, 4};
 	int setB[] = {5, 6, 7, 8};
 
 	Set *A, *B;
 	A = initSet(setA, 4);
 	B = initSet(setB, 4);
+
 	printSet(A);
 	printf("\n");
 	printSet(B);
@@ -88,6 +136,14 @@ int main (int argc, char **argv) {
 	CartProduct *cart = initCartesian(A, B);
 
 	printCartesian(cart);
+	printf("\n");
+
+	for (int i = 0; i <cart->size; ++i) {
+		insert(hashTable, cart->data[i].x, cart->data[i].x);
+		insert(hashTable, cart->data[i].y, cart->data[i].y);
+	}
+
+	printHashTable(hashTable);
 	printf("\n");
 
 	// free previously allocated memory
